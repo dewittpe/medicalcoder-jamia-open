3162f04394cdf11a9ac1f3d8e2814cac4d171c72
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2026-02-03 14:23:51 -0700

    explictly use namespaces when building figure 1

--- figure1-flag-method-example.R
+++ figure1-flag-method-example.R
@@ -22,10 +22,6 @@
 #   run from repo root so outputs land alongside manuscript assets
 #
 ################################################################################
-library(data.table)
-library(medicalcoder)
-library(ggplot2)
-library(ggh4x)
 
 # assume we have a patient record for six encounters.  We use ICD-10 diagnostic
 # codes C78.4 and I50.40 which maps to a cancer and heart failure
@@ -34,25 +30,25 @@ library(ggh4x)
 # I50.40 intentionally marked as not present on admission.
 # POA (present on admission) is varied to show algorithm differences.
 
-lookup_icd_codes(c("C78.4", "I50.40"))
+medicalcoder::lookup_icd_codes(c("C78.4", "I50.40"))
 
 codes <- c("C78.4", "I50.40")
 cols  <- c("icdv", "dx", "code", "full_code", "condition")
 
 subset(
-  get_pccc_codes(),
+  medicalcoder::get_pccc_codes(),
   subset = full_code %in% codes,
   select = c(cols, "pccc_v3.1")
 )
 
 subset(
-  get_charlson_codes(),
+  medicalcoder::get_charlson_codes(),
   subset = full_code %in% codes,
   select = c(cols, "charlson_quan2005")
 )
 
 subset(
-  get_elixhauser_codes(),
+  medicalcoder::get_elixhauser_codes(),
   subset = full_code %in% codes & elixhauser_ahrq2025 == 1L,
   select = c(cols, "elixhauser_ahrq2025")
 )
@@ -87,7 +83,7 @@ args <-
     sorted = FALSE
   )
 # NA poa => use poa.var; non-NA poa => use static POA flag
-args[, poa.var := fifelse(is.na(poa), "poa", NA_character_)]
+args[, poa.var := data.table::fifelse(is.na(poa), "poa", NA_character_)]
 args[!startsWith(method, "pccc"), primarydx := 0L]
 
 build_args <-
@@ -100,7 +96,7 @@ build_args <-
 
 args[, args := apply(.SD, 1, build_args)]
 
-args[, rtn := lapply(args, do.call, what = comorbidities)]
+args[, rtn := lapply(args, do.call, what = medicalcoder::comorbidities)]
 
 tab <- list()
 for (i in seq_len(nrow(args))) {
@@ -113,17 +109,18 @@ for (i in seq_len(nrow(args))) {
   } else {
     m <- m[, .(patid, encid, CVD = HF, CANCER = CANCER_METS)]
   }
-  m[, poa := fifelse(is.na(args[["poa"]][[i]]), "v", as.character(args[["poa"]][[i]]))]
+  m[, poa := data.table::fifelse(is.na(args[["poa"]][[i]]), "v", as.character(args[["poa"]][[i]]))]
   m[, flag.method := args[["flag.method"]][[i]]]
   m[, method := args[["method"]][[i]]]
   # annotate encounter codes; * indicates POA = 1 for that code
   m[, codes := c("", "C78.4", "I50.40*", "", paste0("C78.4*\nI50.40"), "")]
   tab[[i]] <- m
 }
-tab <- rbindlist(tab)
+tab <- data.table::rbindlist(tab)
 
 tab <-
-  melt(tab,
+  data.table::melt(
+    data = tab,
     measure.vars = c("CVD", "CANCER"),
     variable.factor = FALSE
   )
@@ -136,30 +133,30 @@ tab[, poa    := factor(poa, c("0", "1", "v"), labels = c("poa = 0", "poa = 1", "
 tab[, method := factor(method, c("charlson_quan2005", "elixhauser_ahrq2025", "pccc_v3.1"), c("Charlson\n(Quan 2005)", "Elixhauser\n(AHRQ 2025)", "PCCC\nv3.1"))]
 
 g <-
-  ggplot(tab) +
-  theme_bw() +
-  aes(x = flag.method, y = encid) +
-  geom_point(mapping = aes(shape = value), size = 2) +
-  scale_shape_manual(values = c("Not Flagged" = NULL, "Flagged" = 13)) +
-  geom_text(
+  ggplot2::ggplot(tab) +
+  ggplot2::theme_bw() +
+  ggplot2::aes(x = flag.method, y = encid) +
+  ggplot2::geom_point(mapping = ggplot2::aes(shape = value), size = 2) +
+  ggplot2::scale_shape_manual(values = c("Not Flagged" = NULL, "Flagged" = 13)) +
+  ggplot2::geom_text(
     data  = tab[, unique(.SD), .SDcols = c("encid", "codes")],
-    mapping = aes(x = 1.5, y = encid, label = codes),
+    mapping = ggplot2::aes(x = 1.5, y = encid, label = codes),
     size = 2
   ) +
   # facet by method, POA handling, and condition
-  facet_nested(method ~ poa + variable) +
-  ylab("Encounter") +
-  xlab("Flag Method") +
-  labs(
+  ggh4x::facet_nested(method ~ poa + variable) +
+  ggplot2::ylab("Encounter") +
+  ggplot2::xlab("Flag Method") +
+  ggplot2::labs(
     caption = "* Present on Admission\nC78.4 does not need to be POA to be flagged by Elixhauser\nI50.40 does need to be POA to be flagged by Elixhauser"
   ) +
-  theme(
+  ggplot2::theme(
     legend.position = "none"
   )
 
 # figure output sizes aligned with manuscript layout
-ggsave(plot = g, filename = "figure1.png", width = 8, height = 4.5)
-ggsave(plot = g, filename = "figure1.svg", width = 8, height = 4.5)
+ggplot2::ggsave(plot = g, filename = "figure1.png", width = 8, height = 4.5)
+ggplot2::ggsave(plot = g, filename = "figure1.svg", width = 8, height = 4.5)
 
 # save intermediate data for reproducibility/debugging
 save(record, args, file = "figure1.Rdata")
