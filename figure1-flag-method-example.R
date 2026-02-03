22bf1483d063cde8714bd7fab59310f4a74beb33
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2026-02-03 14:12:46 -0700

    add more comments to script for building figure 1

--- figure1-flag-method-example.R
+++ figure1-flag-method-example.R
@@ -1,8 +1,25 @@
 ################################################################################
 # file: figure1-flag-method-example.R
 #
-# build a table/figure showing when a condition is flagged based on present on
-# admission codes and comorbidity algorithm
+# objective:
+#
+#   build a table/figure showing when a condition is flagged based on
+#   present on admission codes and comorbidity algorithm
+#
+# inputs:
+#
+#   none (uses a hard-coded example record)
+#
+# outputs:
+#
+#   figure1.png, figure1.svg, figure1.Rdata (record + args)
+#
+# dependencies:
+#
+#  data.table, medicalcoder, ggplot2, ggh4x
+#
+# Notes:
+#   run from repo root so outputs land alongside manuscript assets
 #
 ################################################################################
 library(data.table)
@@ -15,6 +32,7 @@ library(ggh4x)
 # (cardiovascular disease) comorbidity respectively for PCCC, Charlson, and
 # Elixhauser. For demonstration, we also flag POA with the second report of
 # I50.40 intentionally marked as not present on admission.
+# POA (present on admission) is varied to show algorithm differences.
 
 lookup_icd_codes(c("C78.4", "I50.40"))
 
@@ -42,6 +60,7 @@ subset(
 record <-
   structure(
     list(
+      # 6 encounters for 1 patient; NA code/POA rows represent no code that visit
       patid = c("A", "A", "A", "A", "A", "A", "A"),
       encid = c(1L, 2L, 3L, 4L, 5L, 5L, 6L),
       code = c(NA, "C78.4", "I50.40", NA, "C78.4", "I50.40", NA),
@@ -67,11 +86,13 @@ args <-
     flag.method = c("current", "cumulative"),
     sorted = FALSE
   )
+# NA poa => use poa.var; non-NA poa => use static POA flag
 args[, poa.var := fifelse(is.na(poa), "poa", NA_character_)]
 args[!startsWith(method, "pccc"), primarydx := 0L]
 
 build_args <-
   function(x) {
+    # remove NA args so poa.var is passed only when needed
     i <- which(is.na(x))
     x[i] <- NULL
     x
@@ -84,6 +105,7 @@ args[, rtn := lapply(args, do.call, what = comorbidities)]
 tab <- list()
 for (i in seq_len(nrow(args))) {
   m <- args[["rtn"]][[i]]
+  # normalize method-specific outputs to CVD/CANCER for plotting
   if (startsWith(args[["method"]][[i]], "pccc")) {
     m <- m[, .(patid, encid, CVD = cvd_dxpr_or_tech, CANCER = malignancy_dxpr_or_tech)]
   } else if (startsWith(args[["method"]][[i]], "charlson")) {
@@ -94,6 +116,7 @@ for (i in seq_len(nrow(args))) {
   m[, poa := fifelse(is.na(args[["poa"]][[i]]), "v", as.character(args[["poa"]][[i]]))]
   m[, flag.method := args[["flag.method"]][[i]]]
   m[, method := args[["method"]][[i]]]
+  # annotate encounter codes; * indicates POA = 1 for that code
   m[, codes := c("", "C78.4", "I50.40*", "", paste0("C78.4*\nI50.40"), "")]
   tab[[i]] <- m
 }
@@ -123,6 +146,7 @@ g <-
     mapping = aes(x = 1.5, y = encid, label = codes),
     size = 2
   ) +
+  # facet by method, POA handling, and condition
   facet_nested(method ~ poa + variable) +
   ylab("Encounter") +
   xlab("Flag Method") +
@@ -133,9 +157,11 @@ g <-
     legend.position = "none"
   )
 
+# figure output sizes aligned with manuscript layout
 ggsave(plot = g, filename = "figure1.png", width = 8, height = 4.5)
 ggsave(plot = g, filename = "figure1.svg", width = 8, height = 4.5)
 
+# save intermediate data for reproducibility/debugging
 save(record, args, file = "figure1.Rdata")
 
 ################################################################################
