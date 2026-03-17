d2104775546f7bce81728c30fcee0d932d9f0458
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2026-03-17 13:27:47 -0600

    wip

--- figure2.R
+++ figure2.R
@@ -1,167 +1,128 @@
 objs <- readRDS("objs.rds")
 
-# build the needed data sets for plotting
-flg_charlson <-
-  data.table::rbindlist(
-    list(
-      current    = objs$medicalcoder_charlson_current,
-      cumulative = objs$medicalcoder_charlson_cumulative
-    ),
-    idcol = "flag.method"
-  )
-flg_elixhauser <-
-  data.table::rbindlist(
-    list(
-      current    = objs$medicalcoder_elixhauser_current,
-      cumulative = objs$medicalcoder_elixhauser_cumulative
-    ),
-    idcol = "flag.method"
-  )
-flg_pcccv3.1 <-
-  data.table::rbindlist(
-    list(
-      current    = objs$medicalcoder_pcccv3.1_current,
-      cumulative = objs$medicalcoder_pcccv3.1_cumulative
-    ),
-    idcol = "flag.method"
+flgmthddeltas <- function(current, cumulative) {
+  stopifnot(
+    inherits(current, "data.table"),
+    inherits(cumulative, "data.table")
   )
 
-# remove some rows which are not of interest for this graphic
-flg_charlson[, cci := NULL]
-flg_charlson[, age_score := NULL]
-flg_charlson[, num_cmrb := NULL]
+  DT <-
+    data.table::rbindlist(
+      list(current = current, cumulative = cumulative),
+      idcol = "flag.method"
+    )
 
-flg_elixhauser[, readmission_index := NULL]
-flg_elixhauser[, mortality_index := NULL]
-flg_elixhauser[, num_cmrb := NULL]
+  # get the unique rows without considering the flag method
+  DT <- unique(DT, by = names(DT)[-1])
 
-for(j in grep("_dxpr_and_tech|_dxpr_only|_tech_only", names(flg_pcccv3.1), value = TRUE)) {
-  data.table::set(flg_pcccv3.1, j = j, value = NULL)
+  # if there is a "current" row and no "cumulative" row then there was nothing
+  # different between the two flag methods
+  DT <-
+    data.table::dcast(
+      data = DT,
+      formula = subject_id + enc_seq ~ flag.method,
+      value.var = list("num_cmrb", "cmrb_flag")
+    )
+  DT[, .N, keyby = .(cmrb_flag_current, cmrb_flag_cumulative)]
+  DT <- DT[!is.na(cmrb_flag_cumulative)]
+  DT[, .N, keyby = .(cmrb_flag_current, cmrb_flag_cumulative)]
+
+  # sanity check
+  stopifnot(
+    DT[, all(enc_seq > 1)],
+    DT[cmrb_flag_current == 1 & cmrb_flag_cumulative == 1 & (num_cmrb_current > num_cmrb_cumulative), .N == 0L]
+  )
+
+  DT <-
+    rbind(
+      # case 1: different risk profile
+      DT[, .(case = 1, subjects = data.table::uniqueN(subject_id), encounters = .N)]
+    ,
+      # case 2: flag at least one comorbidit longitudinally when encounter level says zero
+      DT[cmrb_flag_current == 0 & cmrb_flag_cumulative == 1, .(case = 2, subjects = data.table::uniqueN(subject_id), encounters = .N)]
+    ,
+      # case 3: Severity delta only
+      DT[cmrb_flag_current == 1 & cmrb_flag_cumulative == 1 & (num_cmrb_current == num_cmrb_cumulative), .(case = 3, subjects = data.table::uniqueN(subject_id), encounters = .N)]
+    ,
+      # case 4: severity and/or more comorbidities
+      DT[cmrb_flag_current == 1 & cmrb_flag_cumulative == 1 & (num_cmrb_current <= num_cmrb_cumulative), .(case = 4, subjects = data.table::uniqueN(subject_id), encounters = .N)]
+    )[,
+      `:=`(
+        subjects_with_a_cmrb_flag   = (cumulative[enc_seq > 1 & cmrb_flag == 1L, data.table::uniqueN(subject_id)]),
+        encounters_with_a_cmrb_flag = (cumulative[enc_seq > 1 & cmrb_flag == 1L, data.table::uniqueN(.SD, by = c("subject_id", "enc_seq"))])
+      )
+    ][
+      ,
+      `:=`(
+        sp = subjects / subjects_with_a_cmrb_flag,
+        ep = encounters / encounters_with_a_cmrb_flag
+      )
+    ]
+  DT
 }
-flg_pcccv3.1[, num_cmrb := NULL]
 
-# melt all the data sets
-flg_charlson <-
-  data.table::melt(
-    data = flg_charlson,
-    id.vars = c("flag.method", "subject_id", "enc_seq")
-  )
-flg_elixhauser <-
-  data.table::melt(
-    data = flg_elixhauser,
-    id.vars = c("flag.method", "subject_id", "enc_seq")
-  )
-flg_pcccv3.1 <-
-  data.table::melt(
-    data = flg_pcccv3.1,
-    id.vars = c("flag.method", "subject_id", "enc_seq")
-  )
-
-
-# aggregate
-flg_charlson   <- flg_charlson[, .(N = sum(value)), by = .(flag.method, variable)]
-flg_elixhauser <- flg_elixhauser[, .(N = sum(value)), by = .(flag.method, variable)]
-flg_pcccv3.1   <- flg_pcccv3.1[, .(N = sum(value)), by = .(flag.method, variable)]
-
-# extract cmrb_flag
-cmrb_flags <-
+plotdata <-
   data.table::rbindlist(
     list(
-      charlson   = flg_charlson[variable == "cmrb_flag"],
-      elixhauser = flg_elixhauser[variable == "cmrb_flag"],
-      pcccv3.1   = flg_pcccv3.1[variable == "cmrb_flag"]
+      "Charlson"   = flgmthddeltas(current = objs$medicalcoder_charlson_current,   cumulative = objs$medicalcoder_charlson_cumulative),
+      "Elixhauser" = flgmthddeltas(current = objs$medicalcoder_elixhauser_current, cumulative = objs$medicalcoder_elixhauser_cumulative),
+      "PCCC v3.1"  = flgmthddeltas(current = objs$medicalcoder_pcccv3.1_current,   cumulative = objs$medicalcoder_pcccv3.1_cumulative)
     ),
     idcol = "method"
-  ) |> droplevels()
-data.table::setnames(cmrb_flags, old = "method", new = "condition")
-cmrb_flags[, condition := factor(condition, levels = c("charlson", "elixhauser", "pcccv3.1"))]
-
-flg_charlson   <- flg_charlson[variable != "cmrb_flag"]
-flg_elixhauser <- flg_elixhauser[variable != "cmrb_flag"]
-flg_pcccv3.1   <- flg_pcccv3.1[variable != "cmrb_flag"]
-
-# merge on useful descriptions for the graphic
-flg_charlson <-
-  merge(
-    x = flg_charlson,
-    y = medicalcoder::get_charlson_index_scores()[c("condition", "condition_description")],
-    all.x = TRUE,
-    by.x = "variable",
-    by.y = "condition"
   )
 
-data.table::setnames(flg_charlson, old = "condition_description", new = "condition")
-#flg_charlson[condition == "Diabetes with chronic complications",    condition := "DM with\nchronic complications"]
-#flg_charlson[condition == "Diabetes without chronic complications", condition := "DM without\nchronic complications"]
-flg_charlson[, condition := factor(condition, levels = rev(sort(unique(condition))))]
+objs$medicalcoder_charlson_cumulative[cmrb_flag == 1, data.table::uniqueN(.SD, by = c("subject_id"))]
+objs$medicalcoder_charlson_cumulative[cmrb_flag == 1, data.table::uniqueN(.SD, by = c("subject_id", "enc_seq"))]
 
-data.table::setnames(flg_elixhauser, old = "variable", new = "condition")
-flg_elixhauser[condition == "CARDIAC_ARRHYTHMIAS", condition := "CARDIAC\nARRHYTHMIAS"]
-flg_elixhauser[, condition := factor(condition, levels = rev(sort(unique(as.character(condition)))))]
-
-flg_pcccv3.1[, variable := sub("_dxpr_or_tech", "", variable)]
-
-flg_pcccv3.1 <-
-  merge(
-    x = flg_pcccv3.1,
-    y = unique(medicalcoder::get_pccc_conditions()[c("condition", "condition_label")]),
-    all.x = TRUE,
-    by.x = "variable",
-    by.y = "condition"
-  )
-
-flg_pcccv3.1[, condition := data.table::fcase(
-  variable == "any_tech_dep", "Technology Dependence",
-  variable == "any_transplant", "Transplant",
-  startsWith(condition_label, "Miscellaneous"), "Miscellaneous",
-  default = condition_label
+plotdata[, case := factor(
+  x = case,
+  levels = c(1, 2, 3, 4),
+  labels =
+    c(
+      "Different\nRisk Profile",
+      "Zero Encounter-level Comorbidity\n≥ 1 Longitudinal Comorbidities",
+      "Only Differences in Classification",
+      "Differences in Classification\nand/or\nAdditional Comorbidities"
+    )
   )
 ]
-flg_pcccv3.1[, condition := factor(condition, levels = rev(sort(unique(condition))))]
 
+plotdata <- data.table::melt(data = plotdata, measure.vars = c("sp", "ep"))
 
-# build graphic
-g <- function(DT) {
+data.table::setkey(plotdata, method, case, variable)
+
+g <- function(DT, N = "subjects_with_a_cmrb_flag") {
   ggplot2::ggplot(data = DT) +
-    ggplot2::aes(x = condition, y = N, fill = flag.method) +
-    ggplot2::geom_col(position = ggplot2::position_dodge()) +
-    ggplot2::scale_fill_manual(
-      name = "Flag Method",
-      values = c("current" = "#6F263D", "cumulative" = "#4F8FCB")
+    ggplot2::aes(
+      x = sprintf("%s\n(%s %s)", method, formatC(DT[[N]], format = "d", big.mark = ","), ifelse(startsWith(N, "subject"), "Subjects", "Encounters")),
+      y = value,
+      fill = case) +
+    ggplot2::geom_col(
+      position = ggplot2::position_dodge()
     ) +
-    ggplot2::scale_y_continuous(
-      name = "Encounters",
-      label = scales::label_comma()
-    ) +
+    ggplot2::scale_y_continuous(label = scales::label_percent()) +
+    ggplot2::scale_fill_brewer(type = "qual", palette = "Dark2") +
     ggplot2::theme_bw(base_size = 12) +
     ggplot2::theme(
-      axis.title.y = ggplot2::element_blank(),
-      legend.position = "bottom",
-    ) +
-    ggplot2::coord_flip()
+      axis.title = ggplot2::element_blank(),
+      legend.title = ggplot2::element_blank(),
+      legend.key.height = grid::unit(1/8, "npc")
+    )
 }
+
+a <- g(plotdata[variable == "sp"], N = "subjects_with_a_cmrb_flag")# + ggplot2::xlab("Algorithm (Subjects with at least one encounter with at least one comorbidity)")
+b <- g(plotdata[variable == "ep"], N = "encounters_with_a_cmrb_flag")# + ggplot2::xlab("Algorithm (Encounters with at least one comorbidity)")
 
 out <-
   ggpubr::ggarrange(
-    ggpubr::ggarrange(
-      g(flg_elixhauser) + ggplot2::facet_wrap(~ "Elixhauser (Quan 2005)"),
-      ggpubr::ggarrange(
-        g(flg_charlson)   + ggplot2::facet_wrap(~ "Charslon (Quan 2005)"),
-        g(flg_pcccv3.1)   + ggplot2::facet_wrap(~ "PCCC v3.1 (Feinstein 2024)"),
-        labels = c("(B)", "(C)"),
-        legend = "none",
-        ncol = 1
-      ),
-    labels = c("(A)", ""),
-    legend = FALSE,
-    ncol = 2
-  ),
-  g(cmrb_flags) + ggplot2::facet_wrap(~ "Any Comorbidity"),
-  labels = c("", "(D)"),
-  common.legend = TRUE,
-  ncol = 1,
-  heights = c(5,1)
-)
+    a, b,
+    ncol = 1,
+    labels = c("(A)", "(B)"),
+    label.x = 0.04,
+    label.y = 0.95,
+    common.legend = TRUE,
+    legend = "right"
+  )
 
 ggplot2::ggsave(
   filename = "figure2.svg",
@@ -169,6 +130,8 @@ ggplot2::ggsave(
   width = 12,
   height = 9
 )
+
+saveRDS(plotdata, file = "figure2data.rds")
 
 ################################################################################
 #                                 End of File                                  #
